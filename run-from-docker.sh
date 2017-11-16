e3f0eb82247ef19e2371edef4a55bccb9c684f58
Ilya <iproha94@gmail.com>
2017-11-16 19:12:58 +0300

    refact

--- run-from-docker.sh
+++ run-from-docker.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+if [ ! -n "${PATH_TO_SYSBENCH}" ]; then PATH_TO_SYSBENCH=$(pwd); fi
+
 # Build Tarantool
 cd /tarantool
 git pull
-if [ -n "${BRANCH}" ]; then git checkout ${BRANCH}; fi
-branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
-branch_name=${branch_name##refs/heads/}
+if [ ! -n "${BRANCH}" ]; then BRANCH="1.8"; fi
+git checkout ${BRANCH};
 cmake . -DENABLE_DIST=ON; make; make install
+
+# define tarantool version
+cd ${PATH_TO_SYSBENCH}
+TAR_VER=$(tarantool -v | grep -e "Tarantool" |  grep -oP '\s\K\S*')
+echo ${TAR_VER}"["${BRANCH}"]" | tee version.txt
 
 # Build tarantool-c
 cd /tarantool-c
@@ -14,57 +20,14 @@ git pull
 cd third_party/msgpuck/; cmake . ; make; make install; cd ../..
 cmake . ; make; make install
 
-# Build tpcc
+# Build sysbench
 cd ${PATH_TO_SYSBENCH}
 ./autogen.sh; ./configure --with-tarantool --without-mysql; make; make install;
 
 # Run Tarantool
-cd ${PATH_TO_SYSBENCH}
-tarantool sysbench-server.lua
-
-STATUS="$(echo box.info.status | tarantoolctl connect /usr/local/var/run/tarantool/sysbench-server.control | grep -e "- running")"
-while [ ${#STATUS} -eq "0" ]; do
-    echo "waiting load snapshot to tarantool..."
-    sleep 5
-    STATUS="$(echo box.info.status | tarantoolctl connect /usr/local/var/run/tarantool/sysbench-server.control | grep -e "- running")"
-done
-cat /usr/local/var/log/tarantool/sysbench-server.log
+PATH_TO_SYSBENCH=${PATH_TO_SYSBENCH} ./run-tarantool-server.sh
 
 # Run SysBench
-ARRAY_TESTS=(
-    "oltp_read_only"
-    "oltp_point_select"
-    "oltp_insert"
-    "oltp_update_index"
-    "oltp_update_non_index"
-    "select_random_points"
-    "select_random_ranges"
-    "bulk_insert"
-    "oltp_write_only"
-    "oltp_read_write"
-    "oltp_delete"
-)
-
 apt-get install -y -f gdb
-rm -f result.txt
-
-TAR_VER=$(tarantool -v | grep -e "Tarantool" |  grep -oP '\s\K\S*')
-echo $TAR_VER"["$branch_name"]" | tee version.txt
-
-if [ ! -n "${TIME}" ]; then TIME=220; fi
-
-for test in "${ARRAY_TESTS[@]}"; do
-    echo "------------" $test "------------"
-
-    sysbench $test --db-driver=tarantool --threads=1 cleanup | tee $test".txt"
-    sysbench $test --db-driver=tarantool --threads=1 prepare | tee -a $test".txt"
-    sysbench $test --db-driver=tarantool --threads=1 --time=${TIME} --warmup-time=10 run | tee -a $test".txt"
-    sysbench $test --db-driver=tarantool --threads=1 cleanup | tee -a $test".txt"
-
-    echo -n $test":" | tee -a result.txt
-    cat $test".txt" | grep -e 'transactions:' | grep -oP '\(\K\S*' | tee -a result.txt
-done
-
-echo "test_name:result[trps]"
-cat result.txt
+TIME=${TIME} ./run-set-tests.sh
 
---
+++ run-set-tests.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+ARRAY_TESTS=(
+    "oltp_read_only"
+    "oltp_write_only"
+    "oltp_read_write"
+    "oltp_update_index"
+    "oltp_update_non_index"
+    "oltp_insert"
+    "oltp_delete"
+    "oltp_point_select"
+    "select_random_points"
+    "select_random_ranges"
+#    "bulk_insert"
+)
+
+if [ ! -n "${TIME}" ]; then TIME=220; fi
+if [ ! -n "${DBMS}" ]; then DBMS="tarantool"; fi
+if [ ! -n "${THREADS}" ]; then THREADS=1; fi
+if [ ! -n "${RESULT_FILE_NAME}" ]; then RESULT_FILE_NAME="result.txt"; fi
+
+if [ -n "${USER}" ]; then USER=--${DBMS}-user=${USER}; fi
+if [ -n "${PASSWORD}" ]; then PASSWORD=--${DBMS}-password=${PASSWORD}; fi
+
+rm -f ${RESULT_FILE_NAME}
+for test in "${ARRAY_TESTS[@]}"; do
+    echo "------------" $test "------------"
+
+    sysbench $test --db-driver=${DBMS} --threads=${THREADS} cleanup | tee $test".txt"
+    sysbench $test --db-driver=${DBMS} --threads=${THREADS} prepare | tee -a $test".txt"
+    sysbench $test --db-driver=${DBMS} --threads=${THREADS} --time=${TIME} --warmup-time=10 run | tee -a $test".txt"
+    sysbench $test --db-driver=${DBMS} --threads=${THREADS} cleanup | tee -a $test".txt"
+
+    echo -n $test":" | tee -a ${RESULT_FILE_NAME}
+    cat $test".txt" | grep -e 'transactions:' | grep -oP '\(\K\S*' | tee -a ${RESULT_FILE_NAME}
+done
+
+echo "test_name:result[trps]"
+cat ${RESULT_FILE_NAME}
