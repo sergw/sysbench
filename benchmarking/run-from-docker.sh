ccc787b76ef1bb7439fd35a730ffc79abf736caa
Ilya <iproha94@tarantool.org>
2017-12-18 12:46:48 +0300

    add param DCMAKE_BUILD_TYPE

--- benchmarking/run-from-docker.sh
+++ benchmarking/run-from-docker.sh
@@ -8,11 +8,12 @@ if [ ! -n "${PATH_TO_TARANTOOL}" ]; then PATH_TO_TARANTOOL="/opt/tarantool"; fi
 # build Tarantool
 if [ ! -n "${BRANCH}" ]; then BRANCH="1.8"; fi
 if [ ! -n "${HEAD_OFFSET}" ]; then HEAD_OFFSET="0"; fi
+if [ ! -n "${DCMAKE_BUILD_TYPE}" ]; then DCMAKE_BUILD_TYPE="RelWithDebInfo"; fi
 cd ${PATH_TO_TARANTOOL}
 git pull
 git checkout ${BRANCH}
 git checkout HEAD~${HEAD_OFFSET}
-cmake . -DENABLE_DIST=ON  -DCMAKE_BUILD_TYPE=Release; make; make install
+cmake . -DENABLE_DIST=ON  -DCMAKE_BUILD_TYPE=${DCMAKE_BUILD_TYPE} ; make; make install
 
 # define tarantool version
 cd ${PATH_TO_BENCHMARKING}
