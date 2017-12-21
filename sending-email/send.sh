9a4366c85f02dfa01ffe23756d6616a4f973086e
Ilya <iproha94@tarantool.org>
2017-12-21 14:32:43 +0300

    use mutt for send email

--- sending-email/send.sh
+++ sending-email/send.sh
@@ -2,13 +2,8 @@
 
 if [ ! -n "${MY_EMAIL}" ]; then exit 0; fi
 
-/etc/init.d/postfix start
-/etc/init.d/postfix status
-sleep 3
-/etc/init.d/postfix status
 cat ./benchmarking/result.txt > letter.txt
 echo "all results:" >> letter.txt
 echo "http://bench.tarantool.org/?tab=tab-sysbench" >> letter.txt
-echo "" >> letter.txt
 
-cat letter.txt | mail -s "sysbench result for $(cat ./benchmarking/version.txt)" ${MY_EMAIL}
+cat letter.txt | mutt -s "sysbench result for $(cat ./benchmarking/version.txt)" -- ${MY_EMAIL}
