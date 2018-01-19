77049ee66fb880ddc9c90e5d357fa73ed65b9e2b
Ilya <iproha94@tarantool.org>
2018-01-19 23:11:30 +0300

    fix pull branch

--- benchmarking/run-from-docker.sh
+++ benchmarking/run-from-docker.sh
@@ -10,15 +10,19 @@ if [ ! -n "${BRANCH}" ]; then BRANCH="1.8"; fi
 if [ ! -n "${HEAD_OFFSET}" ]; then HEAD_OFFSET="0"; fi
 if [ ! -n "${DCMAKE_BUILD_TYPE}" ]; then DCMAKE_BUILD_TYPE="RelWithDebInfo"; fi
 cd ${PATH_TO_TARANTOOL}
-git checkout ${BRANCH}
 git pull origin ${BRANCH}
+git checkout ${BRANCH}
 git checkout HEAD~${HEAD_OFFSET}
 cmake . -DENABLE_DIST=ON  -DCMAKE_BUILD_TYPE=${DCMAKE_BUILD_TYPE} ; make; make install
 
 # define tarantool version
 cd ${PATH_TO_BENCHMARKING}
-TAR_VER=$(tarantool -v | grep -e "Tarantool" |  grep -oP '\s\K\S*')
-echo ${TAR_VER} | tee version.txt
+if [ "${BRANCH}" != "1.8" ]; then
+    echo "0.0.0-0-"${BRANCH} | tee version.txt
+else
+    TAR_VER=$(tarantool -v | grep -e "Tarantool" |  grep -oP '\s\K\S*')
+    echo ${TAR_VER} | tee version.txt
+fi
 
 # run tarantool
 cd ${PATH_TO_BENCHMARKING}
