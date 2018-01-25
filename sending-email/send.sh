95007bd3353a25935a908c4e95e87f2edc10aba7
Ilya <iproha94@tarantool.org>
2018-01-25 21:34:24 +0300

    set email subject

--- sending-email/send.sh
+++ sending-email/send.sh
@@ -2,14 +2,27 @@
 
 if [ ! -n "${EMAIL_LOGIN}" ]; then exit 0; fi
 if [ ! -n "${EMAIL_PASSWORD}" ]; then exit 0; fi
-if [ ! -f "benchmarking/commit-author.txt" ]; then exit 0; fi
 
-EMAIL_RCPT=`cat benchmarking/commit-author.txt`
+if [ -f "benchmarking/commit-author.txt" ]; then
+    EMAIL_RCPT=`cat benchmarking/commit-author.txt`
+else
+    EMAIL_RCPT="${EMAIL_DEFAULT}"
+fi
 
-echo "Hello" > letter.txt
-cat ./benchmarking/result.txt >> letter.txt
-echo "all results:" >> letter.txt
-echo "http://bench.tarantool.org/?tab=tab-sysbench" >> letter.txt
+VERSION=`cat benchmarking/version.txt`
+
+echo "Subject: [benchmarks] $VERSION $BRANCH" > letter.txt
+echo "Hello" >> letter.txt
+
+
+if [ -n "${SUCCESS_BENCHMARK}" ]; then
+    cat ./benchmarking/result.txt >> letter.txt
+    echo "all results:" >> letter.txt
+    echo "http://bench.tarantool.org/?tab=tab-sysbench" >> letter.txt
+else
+    echo "Fail benchmark. See" >> letter.txt
+    echo "https://gitlab.com/tarantool/sysbench/pipelines" >> letter.txt
+fi
 
 curl --connect-timeout 15 -v \
     --insecure "smtp://smtp.mail.ru:2525" \
